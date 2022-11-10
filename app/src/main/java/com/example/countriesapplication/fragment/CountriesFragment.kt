b566f907aaa6300a19c7101eb0b919d911933f7c
Abiodun Salami <abiodunslam@gmail.com>
2022-11-10 23:41:59 +0100

    design implementation

---
+++ app/src/main/java/com/example/countriesapplication/fragment/CountriesFragment.kt
@@ -0,0 +1,47 @@
+package com.example.countriesapplication.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesapplication.R
+import com.example.countriesapplication.databinding.FragmentCountriesBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+
+class CountriesFragment : Fragment() {
+
+    private lateinit var binding: FragmentCountriesBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCountriesBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.languageSelect.setOnClickListener {
+            val languageBottomSheetDialog = BottomSheetDialog(
+                requireContext(),
+                R.style.BottomSheetDialogTheme
+            )
+
+//            val bottomSheetView = LayoutInflater.from(requireContext()).inflate(
+//                R.layout.language_bottom_sheet,
+//            )
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/countriesapplication/fragment/CountryDetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.countriesapplication.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.countriesapplication.R
+import com.example.countriesapplication.databinding.FragmentCountryDetailsBinding
+
+
+class CountryDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentCountryDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCountryDetailsBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+
+
+}
